d24519cd0838eccbfb9dd60aa4101f4700a89fc5
substantialnoninfringinguser <substantialnoninfringinguser@gmail.com>
2009-05-13 05:20:53 +0100

    [svn] Removed troggle from urls Copied from http://cucc@cucc.survex.com/svn/trunk/expoweb/troggle/, rev. 8075 by julian @ 11/8/2008 3:37 PM

--- urls.py
+++ urls.py
@@ -6,15 +6,15 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
-    (r'^troggle/cave/$', caveindex),
-    (r'^troggle/cave/(?P<cave_id>[^/]+)/$', cave),
-    (r'^troggle/cave/(?P<cave_id>[^/]+)/(?P<ent_letter>[^/]?)$', ent),
+    (r'^cave/$', caveindex),
+    (r'^cave/(?P<cave_id>[^/]+)/$', cave),
+    (r'^cave/(?P<cave_id>[^/]+)/(?P<ent_letter>[^/]?)$', ent),
     
-    (r'^troggle/survex/(?P<survex_file>.*)\.index$', index),
-    (r'^troggle/survex/(?P<survex_file>.*)\.svx$', svx),
-    (r'^troggle/survex/(?P<survex_file>.*)\.3d$', threed),
-    (r'^troggle/survex/(?P<survex_file>.*)\.log$', log),
-    (r'^troggle/survex/(?P<survex_file>.*)\.err$', err),
+    (r'^survex/(?P<survex_file>.*)\.index$', index),
+    (r'^survex/(?P<survex_file>.*)\.svx$', svx),
+    (r'^survex/(?P<survex_file>.*)\.3d$', threed),
+    (r'^survex/(?P<survex_file>.*)\.log$', log),
+    (r'^survex/(?P<survex_file>.*)\.err$', err),
 
     (r'^person/$', personindex),
     (r'^person/(.*)$', person),
