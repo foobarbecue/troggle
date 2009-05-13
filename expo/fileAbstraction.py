4ddd12202fc506a8b8c83417d949f924de39ea10
substantialnoninfringinguser <substantialnoninfringinguser@gmail.com>
2009-05-13 05:36:17 +0100

    [svn] Changes to file abstraction, that seemed to make it work by magic
    Copied from http://cucc@cucc.survex.com/svn/trunk/expoweb/troggle/, rev. 8182 by julian @ 1/18/2009 5:07 PM

--- expo/fileAbstraction.py
+++ expo/fileAbstraction.py
@@ -1,21 +1,28 @@
 import troggle.settings as settings
 import os
+import urllib
 
 def urljoin(x, y): return x + "/" + y
 
 def listdir(*path):
     try:
+        strippedpath = [p for p in path if p]
+        root = os.path.join(settings.FILES, *strippedpath )
         l = ""
-        root = os.path.join(settings.FILES, *path)
+        #l = root + "\n"
+        #isdir = os.path.isdir(root)
+        #l += str(isdir) + "\n"
         for p in os.listdir(root):
             if os.path.isdir(os.path.join(root, p)):
                 l += p + "/\n"
+                
             elif os.path.isfile(os.path.join(root, p)):
                 l += p + "\n"
             #Ignore non-files and non-directories
         return l
     except:
-        return urllib.urlopen(settings.FILES + "listdir/" + reduce(urljoin, path))
+        return urllib.urlopen(settings.FILES + "listdir/" + reduce(urljoin, strippedpath))
+
 
 def readFile(*path):
     try:
