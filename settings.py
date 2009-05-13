d43a8e2763149ac428910b678f3c2a9ad245e4d4
substantialnoninfringinguser <substantialnoninfringinguser@gmail.com>
2009-05-13 06:26:17 +0100

    [svn] django-evolution is optional so shouldn't be in main settings Copied from http://cucc@cucc.survex.com/svn/trunk/expoweb/troggle/, rev. 8340 by cucc @ 5/11/2009 3:18 AM

--- settings.py
+++ settings.py
@@ -80,7 +80,7 @@ INSTALLED_APPS = (
     'django.contrib.redirects',
     #'troggle.photologue',
     #'troggle.reversion',
-    'django_evolution',
+    #'django_evolution',
     'troggle.registration',
     'troggle.profiles',
     'troggle.expo',
