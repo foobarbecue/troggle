00f8a02760f8a1b3c8fb4eaa3d6a744bba094c10
substantialnoninfringinguser <substantialnoninfringinguser@gmail.com>
2009-05-13 05:49:29 +0100

    [svn] modify context so it does not pass unneeded and sensitive info in settings to templates Copied from http://cucc@cucc.survex.com/svn/trunk/expoweb/troggle/, rev. 8235 by aaron @ 1/30/2009 6:57 AM

--- context.py
+++ context.py
@@ -1,4 +1,7 @@
 import troggle.settings as settings
 
 def settingsContext(request):
-    return { 'settings':settings }
+    return { 'settings.MEDIA_URL':settings.MEDIA_URL,
+                 'settings.URL_ROOT':settings.URL_ROOT,
+		 'settings.ADMIN_MEDIA_PREFIX':settings.ADMIN_MEDIA_PREFIX,
+		 'settings.SVX_URL':settings.SVX_URL }
