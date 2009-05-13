f5b32ea7ca17cd3b87a83a77ac060af14904bb40
substantialnoninfringinguser <substantialnoninfringinguser@gmail.com>
2009-05-13 05:21:27 +0100

    [svn] Added survex persons to admin site Copied from http://cucc@cucc.survex.com/svn/trunk/expoweb/troggle/, rev. 8078 by julian @ 11/8/2008 11:24 PM

--- expo/admin.py
+++ expo/admin.py
@@ -1,16 +1,26 @@
 from troggle.expo.models import *
 from django.contrib import admin
 
+class RoleInline(admin.TabularInline):
+    model = PersonRole
+    extra = 4
+
+class SurvexBlockAdmin(admin.ModelAdmin):
+    inlines = (RoleInline,)
+
+
 admin.site.register(Cave)
 admin.site.register(Area)
 admin.site.register(OtherCaveName)
 admin.site.register(CaveAndEntrance)
 admin.site.register(SurveyStation)
 admin.site.register(Entrance)
-admin.site.register(SurvexBlock)
+admin.site.register(SurvexBlock, SurvexBlockAdmin)
 admin.site.register(Expedition)
 admin.site.register(Person)
+admin.site.register(PersonRole)
 admin.site.register(PersonExpedition)
+admin.site.register(Role)
 admin.site.register(LogbookEntry)
 admin.site.register(PersonTrip)
 admin.site.register(QM)
