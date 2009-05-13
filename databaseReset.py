fae65932b444bab2842731e5053c2c8d56b187f4
substantialnoninfringinguser <substantialnoninfringinguser@gmail.com>
2009-05-13 05:16:55 +0100

    [svn] Moved and improved reset script from the parsers directory Copied from http://cucc@cucc.survex.com/svn/trunk/expoweb/troggle/, rev. 8052 by julian @ 11/2/2008 11:14 PM

---
+++ databaseReset.py
@@ -0,0 +1,22 @@
+import os
+import settings
+os.environ['PYTHONPATH'] = settings.PYTHON_PATH
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+from django.core import management
+from django.db import connection
+
+cursor = connection.cursor()
+cursor.execute("drop database %s" % settings.DATABASE_NAME)
+cursor.execute("create database %s" % settings.DATABASE_NAME)
+cursor.execute("ALTER DATABASE %s CHARACTER SET=utf8" % settings.DATABASE_NAME)
+cursor.execute("USE %s" % settings.DATABASE_NAME)
+management.call_command('syncdb')
+from django.contrib.auth.models import User
+user = User.objects.create_user('m', 'm@m.com', 'm')
+user.is_staff = True
+user.is_superuser = True
+user.save()
+
+import parsers.cavetab
+import parsers.survex
+#import parsers.logbooks
