c0dc209f6ce279e368775dbf15c006b68053b2c1
substantialnoninfringinguser <substantialnoninfringinguser@gmail.com>
2009-05-20 03:28:48 +0100

    [svn] forgot to add earlier

---
+++ expo/templatetags/link.py
@@ -0,0 +1,8 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter()
+def link(value):
+    return mark_safe("<a href=\'%s\'>"%value.get_absolute_url()+unicode(value)+"</a>")
