ed814c502dce9d7998ecbf4f88a11312f4eb4ae0
Aaron Curtis <aaron.curtis@cantab.net>
2010-04-14 19:54:00 -0600

    Make plotting script do what I want and put back in the related box remover for now.

--- datalogging/plotall.py
+++ datalogging/plotall.py
@@ -19,27 +19,26 @@ tsnum=0
 datemin=None
 datemax=None
 
-dataset=Timeseries.objects.all()[2:7]
+dataset=Timeseries.objects.filter(data_type='air_deg_c')[2:]
 
 for ts in dataset:
     tsnum+=1
     tsls=ts.data()
     
-    ax=fig.add_subplot(5,1,tsnum)
+    ax=fig.add_subplot(len(dataset),1,tsnum)
 
-    ax.xaxis.set_major_locator(months)
-    ax.xaxis.set_major_formatter(monthsFmt)
-    ax.xaxis.set_minor_locator(days)
-    ax.xaxis.set_minor_formatter(daysFmt)    
+    #ax.xaxis.set_major_locator(months)
+    #ax.xaxis.set_major_formatter(monthsFmt)
+    ax.xaxis.set_major_locator(days)
+    ax.xaxis.set_major_formatter(daysFmt)
     print unicode(ts)
-    ax.set_title(unicode(ts.logbook_entry.cave)+' '+ts.location_in_cave)
+    ax.set_title(unicode(ts.logbook_entry.cave)+': '+(ts.location_in_cave or ''))
 
     if not datemin or datemin > min(tsls[0]):
         datemin = min(tsls[0])
     
     if not datemax or datemax < max(tsls[0]):
         datemax = max(tsls[0])
-
 
     ax.plot(tsls[0], tsls[1])
     
