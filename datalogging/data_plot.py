8ea40853f7ab87904eaebafe19d2c20bcd0e4d06
aarongc <aarongc@jacob>
2010-05-31 09:15:03 -0600

    Adding import from seismic data files (SAC) and us army SPAWAR automatic weather station ASCII format.

--- datalogging/data_plot.py
+++ datalogging/data_plot.py
@@ -3,15 +3,20 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from datalogging.models import *
 from core.models import Cave
 from matplotlib import pyplot as plt
-from datetime import datetime
+from datetime import datetime, date
 from django.db.models import Count
 from matplotlib.ticker import MaxNLocator
 
+default_time_range=(datetime(2009,12,8,0,0),datetime(2010,1,3,0,0))
 all_temp_plots=Timeseries.objects.filter(data_type='air_deg_c')
-caves_with_data=Cave.objects.all()
+caves_with_timeseries=Cave.objects.annotate(num_ts=Count('logbookentry__timeseries')).filter(num_ts__gt=0)
+
+#remove 'bogus cave' which was used for testing
+#and some others we don't want displayed
+caves_with_timeseries=caves_with_timeseries.exclude(official_name__in=('Bogus Cave','LEH','Helo Cave'))
 cave_number=0
 
-def time_domain(ts_list=all_temp_plots, date_range=[datetime(2009,12,01),datetime(2010,03,01)]):
+def time_domain(ts_list=all_temp_plots, date_range=default_time_range):
     """
     Produce plots for each timeseries in ts_list (a list or tuple) over the
     range date_range (a 2-tuple of strings in YYYY-MM-DD HH:MM:SS or datetime objects).
@@ -26,23 +31,28 @@ def time_domain(ts_list=all_temp_plots, date_range=[datetime(2009,12,01),datetim
     adjust(plt)
     plt.show()
 
-def crop_date_range(date_range, ts):
+def crop_date_range(date_range_crop, ts):
     """
     Finds the intersection of the input date range with the timeseries start_time and
     end_time.
     """
-    if ts.start_time is not None and date_range[0] < ts.start_time:
-        date_range[0] = ts.start_time
+    date_range_crop=list(date_range_crop)
+    print "input date is:" ,
+    print date_range_crop[0],
+    print "to"
+    print date_range_crop[1],
+    if ts.start_time is not None and date_range_crop[0] < ts.start_time:
+        date_range_crop[0] = ts.start_time
         print "Adjusting start time"
     else:
         print "Didn't need to adjust start time"
-    if ts.end_time is not None and date_range[1] > ts.end_time:
-        date_range[1] = ts.end_time    
+    if ts.end_time is not None and date_range_crop[1] > ts.end_time:
+        date_range_crop[1] = ts.end_time    
         print "Adjusting end time"
     else:
         print "Didn't need to adjust end time"
       
-    return date_range
+    return date_range_crop
 
 def time_domain_do_plot(ts_list, date_range, ax):
     """
@@ -55,22 +65,21 @@ def time_domain_do_plot(ts_list, date_range, ax):
 
         times=ts.datapoint_set.filter(time__range=date_range_cropped).values_list('time',flat=True)
         values=ts.datapoint_set.filter(time__range=date_range_cropped).values_list('value',flat=True)
-        if ts.location_in_cave and ts.logbook_entry.cave:
-            label=unicode(ts.logbook_entry.cave)+': '+ts.location_in_cave
-        elif ts.location_in_cave:
-            label=unicode(ts.logbook_entry.cave)
-        else:
-            label='unknown'
+        label=unicode(ts.location_in_cave)
 
         ax.plot(times, values, label=label)
         ax.set_xlim(date_range)
+        ax.legend(loc='best')
+        ax.yaxis.set_label_text(ts.logbook_entry.cave)
+        ax.yaxis.label.set_rotation('horizontal')
+        ax.yaxis.label.set_ha('right')
         print u'Plotted timeseries: ' + unicode(ts)
 
 def hist_do_plot(ts_list, ax):
     """
     Handles the actual plotting for histogram data.
     """
-    for ts in ts_list:
+    for ts in ts_list and ts.pk !=25:
         
         dps=ts.datapoint_set.all()
         if ts.start_time is not None:
@@ -81,16 +90,16 @@ def hist_do_plot(ts_list, ax):
             ax.hist(dps.values_list('value', flat=True), histtype='step', normed=True, label=ts.location_in_cave)
         except:
             pass
-        ax.set_title(ts.logbook_entry.cave)
+        ax.yaxis.set_label_text(ts.logbook_entry.cave)
 
 
 def adjust(fig):
 #    fig.subplots_adjust(hspace=1)
     for ax in fig.axes:
-        ax.yaxis.set_major_locator(MaxNLocator(2))
+        ax.yaxis.set_major_locator(MaxNLocator(4))
     plt.show()
 
-def time_domain_by_cave(cave_list=all_caves, date_range=[datetime(2009,12,01),datetime(2010,03,01)]):
+def time_domain_by_cave(cave_list=caves_with_timeseries, date_range=default_time_range):
     """
     Produce plots for each timeseries in each cave in cave_list (a list or tuple) over the
     range date_range (a 2-tuple of strings in YYYY-MM-DD HH:MM:SS or datetime objects).
@@ -101,32 +110,41 @@ def time_domain_by_cave(cave_list=all_caves, date_range=[datetime(2009,12,01),da
     fig=plt.figure()
     cave_number=0
     ax=None
-    cave_list=Cave.objects.annotate(num_ts=Count('logbookentry__timeseries')).filter(num_ts__gt=0)
+    
+    #first plot the barometric pressure at LEH
+    ax=fig.add_subplot(len(cave_list),1,1)
+    LEHpress=Timeseries.objects.get(logbook_entry__cave__slug='leh',data_type='press_hpa').datapoint_set
+    ax.plot(LEHpress.values_list('time',flat=True),LEHpress.values_list('value',flat=True))
+    plt.setp(ax.get_xticklabels(), visible=False)
+    ax.set_xlim(date_range)
+    ax.yaxis.set_label_text('Barometric Pressure (hPa)')
+    ax.yaxis.label.set_rotation('horizontal')
+    ax.yaxis.label.set_ha('right')
+    ax.set_ylim(625,640)
+ 
+    #now plot all of the temperatures
     for cave in cave_list:
         print 'Adding subplot for cave: '+unicode(cave)
 
-        #Link every subplot to the xaxis of the first one
-        if ax is not None:
-            ax=fig.add_subplot(len(cave_list)-1,1,cave_number,sharex=ax)
-        else:
-            ax=fig.add_subplot(len(cave_list)-1,1,cave_number)
+        #Link every subplot to the xaxis of the LEH barometric pressure
+        ax=fig.add_subplot(len(cave_list)+1,1,cave_number+2,sharex=ax)
 
         #Hide the x-axis lables, except for the last one
         plt.setp(ax.get_xticklabels(), visible=False)
         if cave_number==(len(cave_list)-1):
-            plt.setp(ax.get_xticklabels(), visible=True)
-        ax.set_title(unicode(cave))            
+            plt.setp(ax.get_xticklabels(), visible=True)    
         time_domain_do_plot(cave.timeseries_set().filter(data_type='air_deg_c'), date_range, ax)
         cave_number+=1
-    
+
     adjust(fig)
+
+    return ax
     
 
-def histograms_by_cave(cave_list=all_caves):
+def histograms_by_cave(cave_list=caves_with_timeseries):
     fig=plt.figure()
     cave_number=0
     ax=None
-    cave_list=Cave.objects.annotate(num_ts=Count('logbookentry__timeseries')).filter(num_ts__gt=0).exclude(official_name='Bogus Cave')
     for cave in cave_list:
         print 'Adding subplot for cave: '+unicode(cave)
 
@@ -147,4 +165,5 @@ def histograms_by_cave(cave_list=all_caves):
     
     adjust(fig)
     
-
+def scatterplots(xtimeseries, ytimeseries_list):
+    pass
